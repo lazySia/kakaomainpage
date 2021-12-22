ffd1abd34c829c4051e877bf0ca3f895168e9c03
lazySia <tldk9640@naver.com>
2021-12-22 16:18:10 +0900

    main

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+function darkMode() {
+  var body = document.body;
+  var date = document.getElementsByClassName("tit_date22");
+  var lightbtn = document.getElementsByClassName("lightbtn");
+  var darkbtn = document.getElementsByClassName("darkbtn");
+  body.classList.toggle("dark-mode");
+  date.classList.toggle("dark-mode");
+  lightbtn.classList.toggle("dpnone");
+  darkbtn.classList.toggle("dpnone");
+}
+
+// function changeBtn() {}
+
+// });
