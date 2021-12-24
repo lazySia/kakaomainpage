6ab8e4d6e509d626659cd6f4781583d3170a1669
lazySia <tldk9640@naver.com>
2021-12-24 14:29:14 +0900

    submenu hover

--- js/index.js
+++ js/index.js
@@ -9,33 +9,3 @@ function darkMode() {
   lightbtn.classList.toggle("dpnone");
   darkbtn.classList.toggle("dpnone");
 }
-
-function submenu1() {
-  var ul1 = document.getElementById("submenu1");
-  var ul2 = document.getElementById("submenu2");
-  var ul3 = document.getElementById("submenu3");
-
-  ul1.classList.toggle("dpnone");
-  ul2.classList.add("dpnone");
-  ul3.classList.add("dpnone");
-}
-
-function submenu2() {
-  var ul1 = document.getElementById("submenu1");
-  var ul2 = document.getElementById("submenu2");
-  var ul3 = document.getElementById("submenu3");
-
-  ul2.classList.toggle("dpnone");
-  ul1.classList.add("dpnone");
-  ul3.classList.add("dpnone");
-}
-
-function submenu3() {
-  var ul1 = document.getElementById("submenu1");
-  var ul2 = document.getElementById("submenu2");
-  var ul3 = document.getElementById("submenu3");
-
-  ul3.classList.toggle("dpnone");
-  ul1.classList.add("dpnone");
-  ul2.classList.add("dpnone");
-}
